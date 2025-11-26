a8510e2935d8c0fa2ab6ee6859947e78748d5fee
Raushan Mourya <146363759+raushanmaurya75@users.noreply.github.com>
2025-11-27 00:56:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let numbers = [10, 5, 20, 15, 8, 2];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum = sum + numbers[i];
+}
+console.log("The sum of all values in the array is:", sum);
